cecd893cd236265d69b0192632fbb44d7aeb7f5f
psiblvdegod <artyombaly@list.ru>
2024-09-28 00:52:07 +0300

    fix

--- vscode/bubbleAndCounting/bubbleAndCountingCode.c
+++ vscode/bubbleAndCounting/bubbleAndCountingCode.c
@@ -4,57 +4,45 @@
 #include <stdbool.h>
 #define ARRAY_SIZE 100000
 
-void swap(int *left, int *right)
-{
+void swap(int *left, int *right) {
     *left = *left + *right;
     *right = *left - *right;
     *left = *left - *right;
 }
 
-int *bubbleSort(int array[], int arraySize)
-{
+int *bubbleSort(int array[], int arraySize) {
     int swapFlag = 0;
-    while (true)
-    {
+    while (true) {
         swapFlag = 0;
-        for (int i = 1; i < arraySize; ++i)
-        {
-            if (array[i] < array[i - 1])
-            {
+        for (int i = 1; i < arraySize; ++i) {
+            if (array[i] < array[i - 1]) {
                 swapFlag = 1;
                 swap(&array[i], &array[i - 1]);
             }
         }
-        if (swapFlag == 0)
-        {
+        if (swapFlag == 0) {
             return array;
         }
     }
 }
 
-int *countingSort(int array[], int arraySize)
-{
+int *countingSort(int array[], int arraySize) {
     int maxElement = 0, minElement = 0;
-    for (int j = 0; j < arraySize; ++j)
-    {
+    for (int j = 0; j < arraySize; ++j) {
         maxElement = ((array[j] > maxElement) ? (array[j]) : (maxElement));
         minElement = ((array[j] < minElement) ? (array[j]) : (minElement));
     }
     const int elementsRange = maxElement - minElement + 1;
     int *amountOfElements = malloc((elementsRange) * sizeof(int));
-    for (int i = 0; i < elementsRange; ++i)
-    {
+    for (int i = 0; i < elementsRange; ++i) {
         amountOfElements[i] = 0;
     }
-    for (int k = 0; k < arraySize; ++k)
-    {
+    for (int k = 0; k < arraySize; ++k) {
         ++amountOfElements[array[k] - minElement];
     }
     int index = 0;
-    for (int q = 0; q < elementsRange; ++q)
-    {
-        for (int w = 0; w < amountOfElements[q]; ++w)
-        {
+    for (int q = 0; q < elementsRange; ++q) {
+        for (int w = 0; w < amountOfElements[q]; ++w) {
             array[index] = q + minElement;
             ++index;
         }
@@ -63,46 +51,37 @@ int *countingSort(int array[], int arraySize)
     return array;
 }
 
-bool bubbleSortTest()
-{
+bool bubbleSortTest() {
     int unsortedArray[] = {5, 4, 2, 1, 3};
     int *sortedArray = bubbleSort(unsortedArray, 5);
-    for (int i = 0; i < 5; ++i)
-    {
-        if (sortedArray[i] != i + 1)
-        {
+    for (int i = 0; i < 5; ++i) {
+        if (sortedArray[i] != i + 1) {
             return false;
         }
     }
     return true;
 }
 
-bool countingSortTest()
-{
+bool countingSortTest() {
     int unsortedArray[] = {-20, 10, 15, -30, 40, -30, -20, 10, 40, -20};
     int *sortedArray = countingSort(unsortedArray, 10);
-    for (int i = 1; i < 10; ++i)
-    {
-        if (sortedArray[i - 1] > sortedArray[i])
-        {
+    for (int i = 1; i < 10; ++i) {
+        if (sortedArray[i - 1] > sortedArray[i]) {
             return false;
         }
     }
     return true;
 }
 
-int main(void)
-{
+int main(void) {
     const bool test = bubbleSortTest() && countingSortTest();
-    if (!test)
-    {
+    if (!test) {
         printf("Error. Tests failed.\n");
         return -1;
     }
     int unsortedArray1[ARRAY_SIZE] = {0};
     int unsortedArray2[ARRAY_SIZE] = {0};
-    for (int i = 0; i < ARRAY_SIZE; ++i)
-    {
+    for (int i = 0; i < ARRAY_SIZE; ++i) {
         unsortedArray1[i] = rand() % ARRAY_SIZE - (ARRAY_SIZE / 2);
         unsortedArray2[i] = rand() % ARRAY_SIZE - (ARRAY_SIZE / 2);
     }
