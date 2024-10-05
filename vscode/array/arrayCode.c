96fa623a2ac607cbac628be9d0433db112d5d971
psiblvdegod <artyombaly@list.ru>
2024-10-05 11:56:49 +0300

    fix

--- vscode/array/arrayCode.c
+++ vscode/array/arrayCode.c
@@ -2,10 +2,8 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-void arrayReversial(int *array, int left, int right)
-{
-    while (left < right)
-    {
+void arrayReversial(int *array, int left, int right) {
+    while (left < right) {
         int tmp = array[left];
         array[left] = array[right];
         array[right] = tmp;
@@ -14,24 +12,19 @@ void arrayReversial(int *array, int left, int right)
     }
 }
 
-bool arrayReversialTest()
-{
+bool arrayReversialTest() {
     int array[] = {1, 2, 3, 4, 5};
     arrayReversial(array, 0, 4);
-    for (int i = 1; i < 5; ++i)
-    {
-        if (array[i] > array[i - 1])
-        {
+    for (int i = 1; i < 5; ++i) {
+        if (array[i] > array[i - 1]) {
             return false;
         }
     }
     return true;
 }
 
-int main(void)
-{
-    if (!arrayReversialTest())
-    {
+int main(void) {
+    if (!arrayReversialTest()) {
         printf("Error. Test failed.\n");
         return 0;
     }
@@ -40,8 +33,7 @@ int main(void)
     scanf("%d%d", &n, &m);
     int *array = malloc((n + m) * sizeof(int));
     printf("Array\n");
-    for (int i = 0; i < n + m; ++i)
-    {
+    for (int i = 0; i < n + m; ++i) {
         array[i] = i;
         printf("%d ", array[i]);
     }
@@ -49,8 +41,7 @@ int main(void)
     arrayReversial(array, n, n + m - 1);
     arrayReversial(array, 0, n + m - 1);
     printf("\nReversed array\n");
-    for (int k = 0; k < n + m; ++k)
-    {
+    for (int k = 0; k < n + m; ++k) {
         printf("%d ", array[k]);
     }
     free(array);
