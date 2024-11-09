60ce43e03b2b24e19d5d4a773a5421b5ee6141bb
psiblvdegod <psiblvdegod@psiblvdegod.local>
2024-11-08 18:19:25 -0800

    nurseryRhyme works i guess

--- nurseryRhyme/theRevenant.c
+++ nurseryRhyme/theRevenant.c
@@ -2,21 +2,26 @@
 #include "list.h"
 
 int positionOfTheRevenant (int mortality, int victimsAmount, bool * errorCode) {
-    List * list = createList(&errorCode);
+    List * list = createList(errorCode);
     if (list == NULL) {
         *errorCode = true;
         return -1;
     }
-    Position mainPosition = NULL;
+    Position creatingListPosition = NULL;
     for (int i = 1; i <= victimsAmount; ++i) {
-        addElement(list, &mainPosition, i, errorCode);
+        addElement(list, &creatingListPosition, i, errorCode);
     }
+    creatingListPosition = NULL;
     Position currentPosition = getFirst(list, errorCode);
     for (int j = 1; listSize(list) > 1; ++j) {
         if (j == mortality) {
-            deleteElement(list, &currentPosition, errorCode);
-            j = 1;
+            Position temp = currentPosition;
+            currentPosition = getNext(currentPosition, errorCode);
+            deleteElement(list, &temp, errorCode);
+            j = 0;
+            continue;
         }
         currentPosition = getNext(currentPosition, errorCode);
     }
+    return getValue(currentPosition, errorCode);
 }
