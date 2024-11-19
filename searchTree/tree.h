e4fa5e7398ec89f54630788b4d35b8cc1c42032d
psiblvdegod <artyombaly@list.ru>
2024-11-19 20:50:16 +0300

    update tree.h

--- searchTree/tree.h
+++ searchTree/tree.h
@@ -18,10 +18,7 @@ Tree * createTree(Node * root, bool * errorCode);
 
 Node * getRoot(Tree * tree);
 
-//if there is a node in the tree with such key, returns this node.
-//else returns a node that can become a parent of the node with such key.
-//does not report which of the cases happened,
-//so requires comparing the keys after call.
+//if there isn't node with such key, returns NULL
 Node * getNodeByKey(Tree * tree, int key, bool * errorCode);
 
 Node * getParentByNode(Tree * tree, Node * node, bool * errorCode);
@@ -30,6 +27,8 @@ void addNode(Tree * tree, Node * newNode, bool * errorCode);
 
 void disposeNode(Tree * tree, Node * deletingNode, bool * errorCode);
 
+//finds suitable parent for new node using it's key.
+//if tree is empty (==NULL) returns NULL
 Node * findSuitablePosition(Tree * tree, int key, bool * errorCode);
 
 
