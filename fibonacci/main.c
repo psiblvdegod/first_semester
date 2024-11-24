063dfe28926edba3feb8a6a5be9a08b4a5c65519
psiblvdegod <psiblvdegod@psiblvdegod.local>
2024-11-24 15:07:37 +0300

    fibonacci final version

--- fibonacci/main.c
+++ fibonacci/main.c
@@ -1,56 +1,40 @@
+#include "fibonacci.h"
 #include <stdio.h>
 #include <stdbool.h>
 #include <time.h>
 
-int fibonacciIterative(int number) {
-    int previousNumber = 0;
-    int currentNumber = 1;
-    for (;number > 1; --number) {
-        int temp = previousNumber + currentNumber;
-        previousNumber = currentNumber;
-        currentNumber = temp;
-    }
-    return currentNumber;
-}
-
-int fibonacciRecursive(int currentNumber) {
-    if (currentNumber == 0 || currentNumber == 1) {
-        return currentNumber;
-    }
-    return fibonacciRecursive(currentNumber - 2) + fibonacciRecursive(currentNumber - 1);
-}
-
-bool test(){
-    return fibonacciIterative(4) == 3 && fibonacciRecursive(4) == 3;
-}
-
-void recursiveTime() {
-    for (int i = 1; i < 10; ++i) {
-        clock_t startTime = clock();
-        fibonacciRecursive(i * 5);
-        const double duration = (double)(clock() - startTime) / CLOCKS_PER_SEC;
-        printf("%d: %lf\n", i * 5, duration);
-    }
-}
-
-void iterativeTime() {
+void printIterativeTime() {
     for (int i = 1; i <= 10; ++i) {
-        clock_t startTime = clock();
-        fibonacciIterative(i * 1000);
+        const clock_t startTime = clock();
+        calculateFibonacciIterative(i * 1000);
         const double duration = (double)(clock() - startTime) / CLOCKS_PER_SEC;
         printf("%d: %lf\n", i * 1000, duration);
     }
 }
 
+void printRecursiveTime() {
+    for (int i = 1; i < 10; ++i) {
+        const clock_t startTime = clock();
+        calculateFibonacciRecursive(i * 5);
+        const double duration = (double)(clock() - startTime) / CLOCKS_PER_SEC;
+        printf("%d: %lf\n", i * 5, duration);
+    }
+}
+
+bool test(){
+    const bool test1 = calculateFibonacciIterative(4) == 3;
+    const bool test2 = calculateFibonacciRecursive(4) == 3;
+    return test1 && test2;
+}
+
 int main(void) {
-    bool testStatus = test();
-    if (!testStatus) {
+    if (!test()) {
         printf("Error. Tests failed.\n");
         return -1;
     }
-    printf("Fibonacci recursive method time\n");
-    recursiveTime();
     printf("Fibonacci iterative method time\n");
-    iterativeTime();
+    printIterativeTime();
+    printf("Fibonacci recursive method time\n");
+    printRecursiveTime();
     printf("Recursive method gets very slow starting with ~45th element.\n");
 }
