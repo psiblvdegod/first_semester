5ce5578892b6be2ff35893892187c0aa025e2eb0
psiblvdegod <@>
2024-12-04 23:53:04 +0300

    fixed expandHashTable(), add comments

--- hashTable/hashTable.h
+++ hashTable/hashTable.h
@@ -3,8 +3,12 @@
 
 typedef List* HashTable;
 
+//if input is not correct returns 0
+//else returns the number obtained by some algorithm.
+//it is not injective mapping to the range from 0 to hashTableSize - 1
 int hashFunction(int hashTableSize, Key key);
 
+//creates table initialized with NULLs
 HashTable createHashTable(int hashTableSize, bool * errorCode);
 
 //if there is element with such key in the table increases frequency.
@@ -18,6 +22,7 @@ void updateHashTable(HashTable hashTable, int hashTableSize, Key key, bool * err
 //changes hashTableSize to size of new hash table (that's why it's int * instead of const int)
 HashTable expandHashTable(HashTable hashTable, int * hashTableSize, bool * errorCode);
 
+//counts not amount of words in text, but amount of unique words
 int countElementsAmount(HashTable hashTable, int hashTableSize, bool * errorCode);
 
 int calculateMaxListLength(HashTable hashTable, int hashTableSize, bool * errorCode);
@@ -26,6 +31,9 @@ double calculateAverageListLength(HashTable hashTable, int hashTableSize, bool *
 
 double calculateFillFactor(HashTable hashTable, int hashTableSize, bool * errorCode);
 
+//if word not in the table returns 0.
+//else returns it's frequency in the text.
 int findFrequencyByKey(HashTable hashTable, int hashTableSize, Key key, bool * errorCode);
 
+//prints all words and it's frequencies in unordered form.
 void printFrequencies(HashTable hashTable, int hashTableSize, bool * errorCode);
--- hashTable/tests.h
+++ hashTable/tests.h
@@ -2,6 +2,8 @@
 #include "hashTable.h"
 #include <stdbool.h>
 
+//checks the correctness of the functions from the module hashTable.c
 bool hashTableTests();
 
+//checks the correctness of the functions from the module list.c
 bool listTests();
