010047d0b02113d967c0bf5ed9158c5eaa717186
psiblvdegod <@>
2024-12-18 23:09:12 +0300

    update main

--- states/main.c
+++ states/main.c
@@ -3,38 +3,12 @@
 #include "tests.h"
 #include "graph.h"
 
-void query_processing(const int user_query, Graph graph, bool *errorCode) {
-    switch(user_query) {
-        case 1:
-            printCapitals(graph);
-            break;
-        case 2:
-            printMatrix(graph);
-            break;
-        case 3:
-            printAdjacencyLists(graph);
-            break;
-        case 4:
-            printf("state?\n");
-            int state = 0;
-            scanf("%d", &state);
-            conquerNearestCity(graph, state, errorCode);
-            break;
-        case 5:
-            printStateAffiliation(graph);
-            break;
-    }
-}
-
 int main(void) {
     bool errorCode = false;
-    graphTests();
-    Graph graph = buildGraph("../text.txt", &errorCode);
-    int user_query = -1;
-    while(user_query) {
-        printf("1 - capitals. 2 - adj matrix. 3 - adj lists\n4 - conquer 5 - state affiliation\n");
-        scanf("%d", &user_query);
-        while(getchar() != '\n');
-        query_processing(user_query, graph, &errorCode);
+    if (!graphTests()) {
+        printf("Error. Tests failed.\n");
+        return -1;
     }
+    Graph graph = buildGraph("../text.txt", &errorCode);
+    printStateAffiliation(graph);
 }
