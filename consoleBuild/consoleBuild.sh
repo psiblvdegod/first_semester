f580edca2a3adf2f07fcc2c489860c6747296738
psiblvdegod <@>
2024-12-20 23:28:14 +0300

    update consoleBuil.sh

--- consoleBuild/consoleBuild.sh
+++ consoleBuild/consoleBuild.sh
@@ -1,29 +1,31 @@
 #sh
 
 mkdir build
-cd ..
+root=$(dirname "$PWD")
+cd $root
 
-# builds all project which have CMakeLists.txt 
+# builds all projects which have CMakeLists.txt 
 for dir in */; do
     if [ -f "$dir/CMakeLists.txt" ]; then
-        mkdir ./consoleBuild/build/$dir
-        cd ./consoleBuild/build/$dir
-        cmake ../../../$dir
+        mkdir consoleBuild/build/$dir
+        cd consoleBuild/build/$dir
+        cmake "$root/$dir"
         make
-        cd ../../../
+        cd $root
     fi
 done
 
-cd ./consoleBuild/build
+cd consoleBuild/build
 
-# runs executables and reports the result
+# runs executables and reports test result
 for dir in */; do
-    cd $dir 
-    ./"${dir%/}"
+    cd $dir
+    name=$(basename "$dir")
+    ./"$name"
     if [ $? -eq 0 ]; then
-        echo "${dir%/} works"
+        echo "$name works"
     else
-        echo "${dir%/} doesnt work"
+        echo "$name doesnt work"
     fi
     cd ..
 done
