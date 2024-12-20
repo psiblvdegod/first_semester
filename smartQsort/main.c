a48e64e750b26b9f93c8aae4f52c5f3c38eeef7f
psiblvdegod <@>
2024-12-20 21:34:01 +0300

    update smartQsort

--- smartQsort/main.c
+++ smartQsort/main.c
@@ -1,27 +1,7 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include "sorting.h"
 #include "tests.h"
 
 int main(void) {
     if (!smartQsortTest() || !insertionSortTest()) {
-        printf("Error. Test failed.\n");
         return -1;
-    }
-    int array[1001] = {0};
-    srand(666);
-    for (int i = 0; i < 1000; ++i) {
-        array[i] = rand() % 2000 - 1000;
-    }
-    bool errorCode = false;
-    smartQsort(array, 0, 1000, &errorCode);
-    if (errorCode) {
-        printf("Something went wrong.\n");
-        return -1;
-    }
-    printf("Sorted random array.\n");
-    for (int k = 0; k < 1000; ++k) {
-        printf("%d ", array[k]);
     }
 }
