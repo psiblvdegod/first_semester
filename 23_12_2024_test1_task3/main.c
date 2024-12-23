77d2f31cf22ad7956469b87265671a3ebfb8708e
psiblvdegod <@>
2024-12-23 12:36:07 +0300

    update error processing

--- 23_12_2024_test1_task3/main.c
+++ 23_12_2024_test1_task3/main.c
@@ -4,7 +4,14 @@
 
 int main(void) {
     int errorCode = 0;
-
+    queueTest(&errorCode);
+    if (errorCode != 0) {
+        return errorCode;
+    }
+    commentsScanTest(&errorCode);
+    if (errorCode != 0) {
+        return errorCode;
+    }
     Queue *queue = scanCommentsFromFile("../text.txt", &errorCode);
     releaseQueue(queue, &errorCode);
     deleteQueue(&queue, &errorCode);
