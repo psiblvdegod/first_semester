a66a2976e93862a7844a05e4590d2ccb83addd3f
psiblvdegod <@>
2025-01-02 17:02:25 +0300

    feat: change signature of functions in test.c|.h

--- parseTree/tests.c
+++ parseTree/tests.c
@@ -1,38 +1,48 @@
+#include "errorCode.h"
 #include "tests.h"
 #include "parse.h"
-#include "errorCode.h"
+#include <stdlib.h>
 
-void stackTests(int *errorCode) {
+int stackTests() {
+    int errorCode = NO_ERRORS;
     Stack stack = NULL;
-    push(&stack, createNode('1', errorCode), errorCode);
-    push(&stack, createNode('2', errorCode), errorCode);
-    push(&stack, createNode('3', errorCode), errorCode);
-    const bool test = getValue(getHead(stack, errorCode), errorCode) == '3';
-    pop(&stack, errorCode);
-    deleteStack(&stack, errorCode);
-    if (*errorCode == NO_ERRORS && !test) {
-        return;
+    push(&stack, createNode('1', &errorCode), &errorCode);
+    push(&stack, createNode('2', &errorCode), &errorCode);
+    push(&stack, createNode('3', &errorCode), &errorCode);
+    const bool getTest1 = getValue(getHead(stack, &errorCode), &errorCode) == '3';
+    pop(&stack, &errorCode);
+    const bool getTest2 = getValue(getHead(stack, &errorCode), &errorCode) == '2';
+    const bool test = getTest1 && getTest2;
+    deleteStack(&stack, &errorCode);
+    if (errorCode == NO_ERRORS && !test) {
+        return TESTS_FAILED_ERROR;
     }
+    return errorCode
 }
 
-void treeTests(int *errorCode) {
-    Node *root = createNode('*', errorCode);;
-    Node *node1 = createNode('+', errorCode);
-    Node *node2 = createNode('-', errorCode);
-    if (*errorCode != NO_ERRORS) {
-        return;
+int treeTests() {
+    int errorCode = NO_ERRORS;
+    Node *root = createNode('*', &errorCode);;
+    Node *node1 = createNode('+', &errorCode);
+    Node *node2 = createNode('-', &errorCode);
+    if (errorCode != NO_ERRORS) {
+        free(root);
+        free(node1);
+        free(node2);
+        return errorCode;
     }
-    addChild(root, node1, left, errorCode);
-    addChild(root, node2, right, errorCode);
-    if (*errorCode != NO_ERRORS) {
-        deleteTree(&root, errorCode);
-        return;
+    addChild(root, node1, left, &errorCode);
+    addChild(root, node2, right, &errorCode);
+    if (errorCode != NO_ERRORS) {
+        deleteTree(&root, &errorCode);
+        return errorCode;
     }
-    const bool getChildTest = getChild(root, left, errorCode) == node1;
-    const bool getValueTest = getValue(root, errorCode) == '*';
+    const bool getChildTest = getChild(root, left, &errorCode) == node1;
+    const bool getValueTest = getValue(root, &errorCode) == '*';
     const bool test = getChildTest && getValueTest;
-    deleteTree(&root, errorCode);
-    if (*errorCode == NO_ERRORS && !test) {
-        *errorCode = TESTS_FAILED_ERROR;
+    deleteTree(&root, &errorCode);
+    if (errorCode == NO_ERRORS && !test) {
+        return TESTS_FAILED_ERROR;
     }
+    return errorCode;
 }
--- parseTree/tests.h
+++ parseTree/tests.h
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 
 // tests functions from stack.c module
-void stackTests(int *errorCode);
+int stackTests();
 
 // tests functions from tree.c module
-void treeTests(int *errorCode);
+int treeTests();
